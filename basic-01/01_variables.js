515257cdd644b3dff46a941e18bd47819973787b
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-08-25 15:28:47 +0000

    comments and some varaible added

--- basic-01/01_variables.js
+++ basic-01/01_variables.js
@@ -1,24 +1,27 @@
 const accountId = 23353
 let accountEmail = "ashafak04@gmail.com"
 var accountPassword = "ahsfakiod%rft5"
-accountCity = "pune"
-
+accountCity = "pune" // javascripe me bina keyword likhe variable declare kar skate hai but aisa nhi karna chahiye .
+let accountState; // yeh "Undefined" print Karega 
+ 
 
 
 // const accountId = 45 it is not possible because const keyword cant be changed after it declared.
 // console.log(accountId);  yeh change nhi hoga q ki variable ka data type const declared hai.
 
-
+//now we want to update and see what happen ....
 accountEmail = "434"
 accountPassword = "34ashfak"
 accountCity = "hyd"
 
 /*
+Prefer not to use var 
+because of issue block scope and functional scope 
 
 
 */
 
 
 
-console.table([accountId, accountEmail,accountPassword,accountCity]) 
+console.table([accountId, accountEmail,accountPassword,accountCity ,accountState]) 
 // If you want to print multiple variable by using single comment then we have to use consol.table([in the square braket write variable name ])
