477a90a7015669f0fe2a23d162b2439e13e7a483
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-08-25 16:16:09 +0000

    container congi done

---
+++ basic-01/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // isse Javascript compailer newer vesion of javascripte use karega ...all code treat like new standard 
+
+
+//alert(3 +5 ) //we are using node.js , not browser if we write the same code in the browser console it will run q ki js ka engine browser ke andar chipa hua hota hai 
+
+
+console.log(6+
+    4)
+console.log("ashfak") // code readiblity should be high , ye code chal to jayega but we have to give proper semicolon and indentation also 
+
+
+// TYPES OF DATA TYPE :-
+
+let name = "ashafak" //
+let age = 34
+let isLoggedIn = false
+
+
+NUMBER => 2 to the power 53
+
+
+
