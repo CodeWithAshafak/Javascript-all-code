16558b7001ce9877163dbb98e247aa0b4726e510
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-08-25 15:17:00 +0000

    container confi done

---
+++ basic-01/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 23353
+let accountEmail = "ashafak04@gmail.com"
+var accountPassword = "ahsfakiod%rft5"
+accountCity = "pune"
+
+
+
+// const accountId = 45 it is not possible because const keyword cant be changed after it declared.
+// console.log(accountId);  yeh change nhi hoga q ki variable ka data type const declared hai.
+
+
+accountEmail = "434"
+accountPassword = "34ashfak"
+accountCity = "hyd"
+
+/*
+
+
+*/
+
+
+
+console.table([accountId, accountEmail,accountPassword,accountCity]) 
+// If you want to print multiple variable by using single comment then we have to use consol.table([in the square braket write variable name ])
