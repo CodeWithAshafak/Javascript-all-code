069466b9ac8ac3d5cba8f22d9db59fc9b4d03a4e
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-08-29 03:23:13 +0000

    all data types with full explaination are covered

--- basic-01/02_datatypes.js
+++ basic-01/02_datatypes.js
@@ -11,12 +11,46 @@ console.log("ashfak") // code readiblity should be high , ye code chal to jayega
 
 // TYPES OF DATA TYPE :-
 
-let name = "ashafak" //
-let age = 34
+let name = "ashafak";//
+console.log(typeof name);
+
+
+let age = 34;
+console.log(typeof age);
+
 let isLoggedIn = false
+console.log(typeof isLoggedIn);
+
+console.log(typeof "ashafk");
 
 
-NUMBER => 2 to the power 53
+
+// TYPE OF DATA TYPE  IN JS :-
+
+//  TYPE -1 :- PREMATIVE DATATYPES :-
+
+
+
+// 1>  NUMBER =>  range 2 to the power 
+// 2> BigInt
+// 3> String => " "
+// 4> Boolean => true/false
+// 5> null => stand alone value ,isse forcefully null raakhte hai , for example 
+      let state = null
+// 6> undefine => variable banay but value assign nhi kiya.
+      let c;
+// 7>  symbol = uniqueness ke bare me baat karne k liye symbol use karte hai 
+
+
+
+// TYPE -2: OBJECT
+
+console.log( typeof undefined) //  "undefined"  hi aayega ans
+console.log(typeof null );  //Ans will be " Object"
+
+
+
+
 
 
 
