1c9e387ad187fbebf76f5915f20e2b918ffb5365
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-08-29 05:05:51 +0000

    coversion compated

--- basic-01/03_conversionOperation.js
+++ basic-01/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = undefined //put strng and number also that is score= "33a" it will throw NaN that is Not a Number ,If socre= undefined then it will throw NaN also ,if score= true it will Thorow 1 and if the score = false  then it will throw 0
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber );
-console.log(valueInNumber);
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber);
 
 
 // Notes
@@ -18,7 +18,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(typeof booleanIsLoggedIn );
+//console.log(typeof booleanIsLoggedIn );
 
 // notes :-
 
@@ -29,9 +29,48 @@ console.log(typeof booleanIsLoggedIn );
 
 let someNumber = 33;
 let stringNumber = String (someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+//console.log(typeof stringNumber);  // here the number is converted into the string
 
-// here the number is converted into the string
+
+
+
+//****************************** Operations **************************************
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%3)
+
+
+
+let str1 = "hello"
+let str2 = " ashafak"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+
+console.log("1" +2); //ans is 12
+console.log(1 + "2"); // ans is 12 
+console.log("1"+ 2 + 3);   // ans is 123
+console.log(1+2+"3");     // ans is 33
+
+
+console.log(+true); //ans is 1
+console.log(+"");   // empty string is 0 then we are going to make positive ans result is 0
+
+
+
+
+
 
 
